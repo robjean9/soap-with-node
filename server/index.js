2f2c65c733d13a873dc00e1fc70e9464bb3e17e3
Robson Jean Penteado <robjean9@hotmail.com>
2020-03-23 21:19:19 -0300

    first

---
+++ server/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var soap = require('soap');
+var http = require('http');
+
+var service = {
+    ws: {
+        calc: {
+          sum : function(args) {
+                var n = 1*args.a + 1*args.b;
+                return { sumResponse : n };
+            },
+
+            multiplicar : function(args) {
+                var n = args.a * args.b;
+                return { mulres : n };
+            }
+        }
+    }
+};
+
+var xml = require('fs').readFileSync('myservice.wsdl', 'utf8');
+
+var server = http.createServer(function(request,response) {
+    response.end("404: Not Found: "+request.url);
+});
+
+server.listen(8001);
+soap.listen(server, '/wscalc1', service, xml);
